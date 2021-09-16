609e8948ae6aec04a4973d67be47482cec77b0a8
djas12 <86751830+djas12@users.noreply.github.com>
2021-09-16 08:43:55 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ plain='\033[0m'
 cur_dir=$(pwd)
 
 # check root
-[[ $EUID -ne 0 ]] && echo -e "${red}错误：${plain} 必须使用root用户运行此脚本！\n" && exit 1
+[[ $EUID -ne 0 ]] && echo -e "${red}Kesalahan：${plain} Kamu harus masuk root untuk menjalankan perintah ini, sampai di sini faham.?！\n" && exit 1
 
 # check os
 if [[ -f /etc/redhat-release ]]; then
@@ -26,7 +26,7 @@ elif cat /proc/version | grep -Eqi "ubuntu"; then
 elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
     release="centos"
 else
-    echo -e "${red}未检测到系统版本，请联系脚本作者！${plain}\n" && exit 1
+    echo -e "${red}versi ini tidak bisa，silahkan hubungi pembuat script！${plain}\n" && exit 1
 fi
 
 arch=$(arch)
@@ -37,13 +37,13 @@ elif [[ $arch == "aarch64" || $arch == "arm64" ]]; then
   arch="arm64"
 else
   arch="amd64"
-  echo -e "${red}检测架构失败，使用默认架构: ${arch}${plain}"
+  echo -e "${red}Gagal mendeteksi arsitektur, gunakan arsitektur default: ${arch}${plain}"
 fi
 
-echo "架构: ${arch}"
+echo "Arsitektur: ${arch}"
 
 if [ $(getconf WORD_BIT) != '32' ] && [ $(getconf LONG_BIT) != '64' ] ; then
-    echo "本软件不支持 32 位系统(x86)，请使用 64 位系统(x86_64)，如果检测有误，请联系作者"
+    echo "Software ini tidak mendukung sistem 32-bit (x86), silakan gunakan sistem 64-bit (x86_64), jika deteksi salah, silakan hubungi penulis"
     exit -1
 fi
 
@@ -59,15 +59,15 @@ fi
 
 if [[ x"${release}" == x"centos" ]]; then
     if [[ ${os_version} -le 6 ]]; then
-        echo -e "${red}请使用 CentOS 7 或更高版本的系统！${plain}\n" && exit 1
+        echo -e "${red}Silahkan gunakan CentOS 7 Atau di atasnya！${plain}\n" && exit 1
     fi
 elif [[ x"${release}" == x"ubuntu" ]]; then
     if [[ ${os_version} -lt 16 ]]; then
-        echo -e "${red}请使用 Ubuntu 16 或更高版本的系统！${plain}\n" && exit 1
+        echo -e "${red}Silahkan gunakan Ubuntu 16 Atau di atasnya！${plain}\n" && exit 1
     fi
 elif [[ x"${release}" == x"debian" ]]; then
     if [[ ${os_version} -lt 8 ]]; then
-        echo -e "${red}请使用 Debian 8 或更高版本的系统！${plain}\n" && exit 1
+        echo -e "${red}Silahkan gunakan Debian 8 Atau di atasnya！${plain}\n" && exit 1
     fi
 fi
 
@@ -84,24 +84,24 @@ install_x-ui() {
     cd /usr/local/
 
     if  [ $# == 0 ] ;then
-        last_version=$(curl -Ls "https://api.github.com/repos/vaxilu/x-ui/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+        last_version=$(curl -Ls "https://api.github.com/repos/djas12/x-ui/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
         if [[ ! -n "$last_version" ]]; then
-            echo -e "${red}检测 x-ui 版本失败，可能是超出 Github API 限制，请稍后再试，或手动指定 x-ui 版本安装${plain}"
+            echo -e "${red}Deteksi x-ui Versi gagal, mungkin lebih dari itu Github API Batasan, silakan coba lagi nanti, atau tentukan secara manual x-ui Instalasi versi${plain}"
             exit 1
         fi
-        echo -e "检测到 x-ui 最新版本：${last_version}，开始安装"
-        wget -N --no-check-certificate -O /usr/local/x-ui-linux-${arch}.tar.gz https://github.com/vaxilu/x-ui/releases/download/${last_version}/x-ui-linux-${arch}.tar.gz
+        echo -e "terdeteksi x-ui Versi terbaru dari：${last_version}，mulai instalasi"
+        wget -N --no-check-certificate -O /usr/local/x-ui-linux-${arch}.tar.gz https://github.com/djas12/x-ui/releases/download/${last_version}/x-ui-linux-${arch}.tar.gz
         if [[ $? -ne 0 ]]; then
-            echo -e "${red}下载 x-ui 失败，请确保你的服务器能够下载 Github 的文件${plain}"
+            echo -e "${red}unduh x-ui gagal，Pastikan server Anda dapat mengunduh Github dokumen${plain}"
             exit 1
         fi
     else
         last_version=$1
-        url="https://github.com/vaxilu/x-ui/releases/download/${last_version}/x-ui-linux-${arch}.tar.gz"
-        echo -e "开始安装 x-ui v$1"
+        url="https://github.com/djas12/x-ui/releases/download/${last_version}/x-ui-linux-${arch}.tar.gz"
+        echo -e "mulai instalasi x-ui v$1"
         wget -N --no-check-certificate -O /usr/local/x-ui-linux-${arch}.tar.gz ${url}
         if [[ $? -ne 0 ]]; then
-            echo -e "${red}下载 x-ui v$1 失败，请确保此版本存在${plain}"
+            echo -e "${red}unduh x-ui v$1 gagal，Pastikan versi ini ada${plain}"
             exit 1
         fi
     fi
@@ -115,36 +115,36 @@ install_x-ui() {
     cd x-ui
     chmod +x x-ui bin/xray-linux-${arch}
     cp -f x-ui.service /etc/systemd/system/
-    wget --no-check-certificate -O /usr/bin/x-ui https://raw.githubusercontent.com/vaxilu/x-ui/main/x-ui.sh
+    wget --no-check-certificate -O /usr/bin/x-ui https://raw.githubusercontent.com/djas12/x-ui/main/x-ui.sh
     chmod +x /usr/bin/x-ui
     systemctl daemon-reload
     systemctl enable x-ui
     systemctl start x-ui
-    echo -e "${green}x-ui v${last_version}${plain} 安装完成，面板已启动，"
+    echo -e "${green}x-ui v${last_version}${plain} Instalasi selesai dan panel telah dimulai，"
     echo -e ""
-    echo -e "如果是全新安装，默认网页端口为 ${green}54321${plain}，用户名和密码默认都是 ${green}admin${plain}"
-    echo -e "请自行确保此端口没有被其他程序占用，${yellow}并且确保 54321 端口已放行${plain}"
-#    echo -e "若想将 54321 修改为其它端口，输入 x-ui 命令进行修改，同样也要确保你修改的端口也是放行的"
+    echo -e "Jika ini adalah instalasi baru, port web default adalah ${green}54321${plain}，Nama pengguna dan kata sandi keduanya secara default ${green}admin${plain}"
+    echo -e "Pastikan port ini tidak ditempati oleh program lain，${yellow}Dan pastikan 54321 telah di buka agar bisa di akses${plain}"
+#    echo -e "Jika kamu ingin 54321 di ubah ke port lain，masuk x-ui lalu cari pengaturan，dan pastikan port yang kalian ubah sudah di buka agar bisa di akses dan tidak eror"
     echo -e ""
-    echo -e "如果是更新面板，则按你之前的方式访问面板"
+    echo -e "Jika ingin memperbarui panel, akses panel seperti yang Anda lakukan sebelumnya"
     echo -e ""
-    echo -e "x-ui 管理脚本使用方法: "
-    echo -e "----------------------------------------------"
-    echo -e "x-ui              - 显示管理菜单 (功能更多)"
-    echo -e "x-ui start        - 启动 x-ui 面板"
-    echo -e "x-ui stop         - 停止 x-ui 面板"
-    echo -e "x-ui restart      - 重启 x-ui 面板"
-    echo -e "x-ui status       - 查看 x-ui 状态"
-    echo -e "x-ui enable       - 设置 x-ui 开机自启"
-    echo -e "x-ui disable      - 取消 x-ui 开机自启"
-    echo -e "x-ui log          - 查看 x-ui 日志"
-    echo -e "x-ui v2-ui        - 迁移本机器的 v2-ui 账号数据至 x-ui"
-    echo -e "x-ui update       - 更新 x-ui 面板"
-    echo -e "x-ui install      - 安装 x-ui 面板"
-    echo -e "x-ui uninstall    - 卸载 x-ui 面板"
+    echo "x-ui Cara menggunakan skrip manajemen: "
+    echo "------------------------------------------"
+    echo "x-ui              - Tampilan menu manajemen (fungsi lainnya)"
+    echo "x-ui start        - Luncurkan panel x-ui"
+    echo "x-ui stop         - Hentikan panel x-ui"
+    echo "x-ui restart      - Mulai ulang panel x-ui"
+    echo "x-ui status       - Lihat status x-ui"
+    echo "x-ui enable       - Atur x-ui untuk memulai secara otomatis setelah boot"
+    echo "x-ui disable      - Batalkan boot x-ui dari awal"
+    echo "x-ui log          - Lihat log x-ui"
+    echo "x-ui v2-ui        - Migrasikan data akun v2-ui mesin ini ke x-ui"
+    echo "x-ui update       - Perbarui panel x-ui"
+    echo "x-ui install      - Instal panel x-ui"
+    echo "x-ui uninstall    - Copot pemasangan panel x-ui"
     echo -e "----------------------------------------------"
 }
 
-echo -e "${green}开始安装${plain}"
+echo -e "${green}mulai instalasi${plain}"
 install_base
 install_x-ui $1

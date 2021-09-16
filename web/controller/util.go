379caea15afb21d2bab647f98d6ddc8feb33cc4c
djas12 <86751830+djas12@users.noreply.github.com>
2021-09-16 08:55:24 +0700

    Update util.go

--- web/controller/util.go
+++ web/controller/util.go
@@ -46,12 +46,12 @@ func jsonMsgObj(c *gin.Context, msg string, obj interface{}, err error) {
 	if err == nil {
 		m.Success = true
 		if msg != "" {
-			m.Msg = msg + "成功"
+			m.Msg = msg + "Sukses"
 		}
 	} else {
 		m.Success = false
-		m.Msg = msg + "失败: " + err.Error()
-		logger.Warning(msg+"失败: ", err)
+		m.Msg = msg + "gagal: " + err.Error()
+		logger.Warning(msg+"gagal: ", err)
 	}
 	c.JSON(http.StatusOK, m)
 }
